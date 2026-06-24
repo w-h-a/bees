4e5fa0fe83e580549fc65b983a5fdd299239721c
Wes Anderson <wesley.herb.anderson@gmail.com>
2026-06-24 14:38:12 -0700

    feat!: flip to device-global store, rm init/config (#84)

--- cmd/bees/comment.go
+++ cmd/bees/comment.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
-func newCommentCmd(cfg **config) *cobra.Command {
+func newCommentCmd() *cobra.Command {
 	var author string
 
 	cmd := &cobra.Command{
@@ -12,8 +14,8 @@ func newCommentCmd(cfg **config) *cobra.Command {
 		Short: "Add a comment to an issue",
 		Args:  cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if author == "" && *cfg != nil {
-				author, _ = resolveConfig(*cfg, "author")
+			if author == "" {
+				author = os.Getenv("BEES_AUTHOR")
 			}
 			return h.Comment(cmd.Context(), cmd.OutOrStdout(), args[0], author, args[1])
 		},
--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/charmbracelet/lipgloss v1.1.0
 	github.com/spf13/cobra v1.10.2
 	github.com/stretchr/testify v1.11.1
-	gopkg.in/yaml.v3 v3.0.1
 	modernc.org/sqlite v1.46.1
 )
 
@@ -32,6 +31,7 @@ require (
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/exp v0.0.0-20251023183803-a4bb9ffd2546 // indirect
 	golang.org/x/sys v0.37.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	modernc.org/libc v1.67.6 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
 	modernc.org/memory v1.11.0 // indirect
